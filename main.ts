5a538ed14537b2649fcb14d6c71d9bf3c238aa42
CaptainCredible <cpt.credible@gmail.com>
2019-06-13 14:41:21 +0200

    added advanced setup to select pin

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ namespace gigabitDisplay {
     /**
 * makes the gigabit display the same as the micro:bit
      */
-    //% block
+    //% blockId="mirrorDisp" block="mirror display on gigaBit"
     export function mirrorDisplayOnGigabit(): void {
         let item = neopixel.create(DigitalPin.P16, 25, NeoPixelMode.RGB)
         control.inBackground(function () {
@@ -25,4 +25,28 @@ namespace gigabitDisplay {
             }
         })
     }
+
+    /**
+* makes the gigabit display the same as the micro:bit on selected pin
+     */
+    //% blockId="mirrorDispAdvanced" block="mirror display on gigaBit connected to pin $pinSelect" advanced=true
+    export function mirrorDisplayOnGigabitAdvanced(pinSelect: DigitalPin): void {
+        let item = neopixel.create(pinSelect, 25, NeoPixelMode.RGB)
+        control.inBackground(function () {
+            while (true) {
+                for (let i = 0; i <= 25 - 1; i++) {
+                    if (led.point(4 - i % 5, i / 5)) {
+                        item.setPixelColor(i, neopixel.colors(NeoPixelColors.Red))
+                    } else {
+                        item.setPixelColor(i, neopixel.colors(NeoPixelColors.Black))
+                    }
+                }
+                basic.pause(10)
+                let brightness = led.brightness()
+                item.setBrightness(brightness)
+                item.show()
+            }
+        })
+    }
+
 }
